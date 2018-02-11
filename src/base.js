fa7a4ef062474eede1ac359c6e936d9f36931098
Ayoub <skobraf@gmail.com>
2018-02-11 18:08:58 +0000

    localestorage

---
+++ src/base.js
@@ -0,0 +1,11 @@
+import Rebase from 're-base';
+
+
+const base  = Rebase.createClass({
+	apiKey: "AIzaSyAMhdXoFRA3HBM8YNmKP4_miB9quyRDMc4",
+    authDomain: "catch-of-the-day-ayoub.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-ayoub.firebaseio.com",
+
+});
+
+export default base;
